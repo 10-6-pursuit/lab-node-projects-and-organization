019ac80217f77506aaa6c81b552ac1306dc634bd
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-06-04 21:29:12 -0400

    complete wordle solver functions and first bonus function

--- index.js
+++ index.js
@@ -85,4 +85,48 @@ function findWordWithNoLetter(letter) {
 // The wordle solver requires two functions:
 
 // One that returns a list of words that has matching letters in any position
+function wordsWithLetterInAnyPosition(letter) {
+    return wordJSON.filter(word => word.includes(letter)).map(word => {
+        indexArray = [] 
+        for (let i = 0; i < word.length; i++) {
+            if(letter === word[i]) {
+                indexArray.push(i)
+            }
+        }
+        return `${word}, position ${[...indexArray]}`
+    })
+}
+// console.log(wordsWithLetterInAnyPosition(process.argv[2]))
+
 // One that returns a list of words that has matching letters that also match the position
+function findWordsWithMatchingLettersInPosition(letter, position) {
+    if(position > 5 || position <= 0) {
+        return "Position must be a number between 1 and 5"
+    }
+    return wordJSON.filter(word => word[position - 1] === letter)
+}
+
+// console.log(findWordsWithMatchingLettersInPosition(process.argv[2], process.argv[3]))
+
+// Bonus
+
+// Write a function lettersMatch that lets you input up to 5 letters in any order and returns the words that match the criteria (don't worry about solving for duplicate letters).
+// node index.js aeiou
+function lettersMatch(letters) {
+    return wordJSON.filter(word => {
+        for (let char of letters) {
+            return word.includes(char)
+        }
+    })
+}
+
+console.log(lettersMatch(process.argv[2]))
+// Write a function letttersExactMatch that lets you input a five-character string. Put letters with the exact placement in their place, and put a placeholder like . or _ for unknown letters.
+// node index.js exactly a_ie_
+
+// Update the function lettersMatch to handle duplicate letters
+
+// Create a new function that can handle both inputs
+// One input for matched letters in the wrong position
+// The second input for matched letters in the correct position
+// node index.js al__a o
