4d3888ce605836e7b18f87dfc8d0bcf0c393ac8c
incognito1025 <nkhanamart@gmail.com>
2024-06-11 06:40:53 -0400

    index.js Start Simple Functions complete

---
+++ index.js
@@ -0,0 +1,153 @@
+const wordsJSON = require("./words.json"); //declare variable, relative path to the file
+//console.log(wordsJSON);
+
+
+//logs all the words 
+//`words` or `arr` param isn't strictly needed in array function
+function allWords(words) {
+   return wordsJSON;
+}
+//console.log(allWords());
+/*
+output:
+[
+  'women', 'nikau', 'swack', 'feens', 
+  ... 12944 more items
+]
+*/
+
+
+
+//logs the first ten words
+const firstTenWords = () => wordsJSON.slice(0, 10);
+//console.log(firstTenWords());
+/*
+output:
+[
+  'women', 'nikau',//
+  'swack', 'feens',
+  'fyles', 'poled',
+  'clags', 'starn',
+  'bindi', 'woops'
+]
+*/
+
+
+//logs the next 10 words.
+const nextTenWords = () => wordsJSON.slice(10, 20);
+//console.log(nextTenWords());
+/*
+output:
+[
+  'fanos', 'cabin',
+  'souct', 'trass',
+  'shoat', 'lefty',
+  'durra', 'hypes',
+  'junta', 'baisa'
+]
+*/
+
+//logs from 0 to x words.
+const firstXWords = (x) => wordsJSON.slice(0, x);
+//console.log(firstXWords(5));
+/*
+output:
+[ 'women', 'nikau', 'swack', 'feens', 'fyles' ]
+*/
+
+//logs from x to y words.
+const subsetOfWords = (x, y) => wordsJSON.slice(x, y);
+//console.log(subsetOfWords(3, 8));
+/*
+output:
+[ 'feens', 'fyles', 'poled', 'clags', 'starn' ]
+*/
+
+//logs all the words, sorted alphabetically.
+const sortWords = () => {
+    return wordsJSON.sort((a, b) => {
+        if (a < b) {
+            return -1;
+        } else if (a > b) {
+            return 1;
+        } else {
+            return 0;
+        }
+    });
+};
+console.log(sortWords());
+/*
+output:
+[ 'aahed', 'aalii', 'aargh', 'aarti',
+  ... 12944 more items ]
+*/
+
+
+
+
+
+/*
+Notes with Mike:
+
+//logs the first 10 words.
+/*
+const firstTenWords = (words) => {
+    return importedWords.slice(0,9); // not correct because index of 9 tells .slice when to stop cutting, so the returned arrary won't include index 9.
+}
+//.slice() array method .slice(start cutting at the beginning index, stop cutting at the end index). Makes a copy of a part of an array without modifying the original array. When it returns copy, that copy does not include element that is the end index, only the one before it.
+//.slice method returns a shallow copy of a section of an array. The start position is the beginning index of the array; if no start position, then automatically start at 0 index. The end index of the specified portion of the array. This is exclusive of the element at the index 'end'. If end is undefined, then the slice extends to the end of the array.
+//mdn - The slice() method is a copying method. It does not alter this but instead return a shallow copy that contains some of the same elements as the ones from the original array. It preserves empty slots.
+
+//const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+//const citrus = fruits.slice(1, 3);
+// fruits contains ['Banana', 'Orange', 'Lemon', 'Apple', 'Mango']
+// citrus contains ['Orange','Lemon']
+
+//online sources- The slice() method extracts a section of the array and returns a new array without modifying the original array. 1st param specifies the index at which to begin the extraction. In JS, array indices start from 0, so `1` refers to the second element("Orange") of the `fruits` array.  The second param(`3`) specifies the index before to which to end the extraction. So, `3` refers to the index of the element after the last element to be extracted, which is "Apple". So, "Apple is not included in the extracted array."
+*/
+
+/*
+//logs the first ten words
+function firstTenWords() {
+    return importedWords.slice(0, 10); //starts at zero, ends at index 10. console will print everything from index 0 to 9 because .slice method does not include the second param index which specifies the end of the extraction, in this case index 10.
+}
+
+//console.log(firstTenWords());
+
+
+//logs the next 10 words.
+//traditional arrow function - const/let (keyword), declare variable (function name), assigning that block of code to run function (expression). If you don't need a parameter, leave parenthesis empty.
+//if block of code is just returning something, (e.g. one-liner), no need for curly brackets. implied return. 
+const nextTenWords = () => importedWords.slice(10, 20); //.slice will return shallow copy of elements from index 10 to index 19. 
+
+
+ //console.log(nextTenWords());
+
+ 
+ //log from 0 to x words.
+ const firstXWords = (x) => importedWords.slice(0, x); //dynamic functions (we don't know what x is). .slice will return everything from index 0 to the element just before index x.
+
+ //console.log(firstXWords(5));
+
+ //logs from x to y words.
+ // .slice starts at param of x, and ends at param, does not return the last element in the shallow copy)
+ const subsetOfWords = (x, y) => importedWords.slice(x, y); //dynamic function
+
+ //console.log(subsetOfWords(8, 13));
+
+
+
+ //.sort method - rearranges elements in a list or array in a specific order (ascending). 
+ //.toSorted method return a new collection with the elements sorted.  It would preserve the original collection while providing a sorted version.
+ //logs all the words, sorted alphabetically.
+ //const sortWords = () => importedWords.toSorted();
+
+//console.log(sortWords());
+
+
+/*
+//JSON file - JavaScript Notation file, a file format used for storing and exchanging data in a structured way. commonly used for configuration files, data storage, and communication between server and client in web apps. 
+
+
+
+*/
